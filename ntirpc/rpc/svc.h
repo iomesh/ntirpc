3b9080663e68a74e668aa458b600c2991f5d584d
Animesh Javali <Animesh.Javali@ibm.com>
2025-01-29 06:35:16 -0500

    Libntirpc code changes for QoS.
    For QoS, two callbacks are added in libntirpc so that these
    callbacks can be called by nfs-ganesha to enable events for
    EPOLLOUT or enable events for EPOLLIN for a socket.
    
    Signed-off-by: Animesh Javali <Animesh.Javali@ibm.com>

--- ntirpc/rpc/svc.h
+++ ntirpc/rpc/svc.h
@@ -314,6 +314,7 @@ struct svc_xprt {
 	int32_t xp_refcnt;	/* handle reference count */
 	uint16_t xp_flags;	/* flags */
 	uint32_t xp_unique_id;
+	bool recv_rearm_allowed;
 
 	union {
 		struct in_pktinfo in;
@@ -406,6 +407,10 @@ struct svc_req {
 #define svc_getrpclocal(x) (&(x)->xp_local.ss)
 
 extern void svc_resume(struct svc_req *req);
+
+extern void svc_rqst_qos_suspend_socket(struct svc_xprt *xprt);
+
+extern void svc_rqst_qos_resume_socket(struct svc_xprt *xprt);
 
 /*
  * Ganesha.  Get connected transport type.
